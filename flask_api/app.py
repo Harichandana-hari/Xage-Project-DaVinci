54529d6bbf41130958d7215144e209c23f4061b5
Harichandana M <mharichandanam@gmail.com>
2024-11-17 23:35:09 +0530

    edit

--- flask_api/app.py
+++ flask_api/app.py
@@ -72,4 +72,4 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':  # Corrected from '_main_' to '__main__'
-    app.run(debug=Falsealse)
+    app.run(debug=False)
